1d8cded577c3b8c9c695e812cdb6f1f2ebe3518b
VTRyo <vtryo.work@gmail.com>
2022-10-26 08:50:41 +0900

    add version cmd

---
+++ cmd/version.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of macro-nutrient-calculator-cli",
+	Long:  `All software has versions. This is macro-nutrient-calculator-cli's'`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("macro-nutrient-calculator-cli v0.1 -- HEAD")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
